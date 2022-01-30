0029242c39ade8672c70fe8f16cb4cf144aec86c
Ramly23 <chowah67@gmail.com>
2022-01-31 00:44:01 +0800

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  reset: true
+})
+
+sr.reveal(`.nav__logo ` , {origin: 'left'})
+sr.reveal(`.nav__links` , {origin: 'right'})
+sr.reveal(`.anywhere__first`, {delay: 500})
+sr.reveal(`.anywhere__second` , {origin: 'right'} , {delay: 600})
+sr.reveal(`.productive__first `)
+sr.reveal(`.productive__second`, {origin: 'right'})
+sr.reveal(`.early__first , .cta , .footer__first, .footer__second ,.footer__third,.footer__fourth` , {interval: 200} , {delay: 700})
